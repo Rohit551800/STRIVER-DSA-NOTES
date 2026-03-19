788bbe604164ae48ddbcf041245bebc6240bcaf5
Rohit551800 <rohitsingh139915@gmail.com>
2026-03-19 22:37:57 +0530

    day4

--- 15 Sliding Window/6noOfSubstringsContainingAllThreeCharacters.cpp
+++ 15 Sliding Window/6noOfSubstringsContainingAllThreeCharacters.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-int bruteforce(string &s , string req){
+int bruteforce(string &s){
     int n = s.size();
     int count = 0;
     for(int i=0;i<n;i++){
-        int hash[req.size()];
+        int hash[3] = {0};
         for(int j=i;j<n;j++){
             hash[s[j] - 'a'] = 1;
             if(hash[0] + hash[1] + hash[2] == 3) {
@@ -30,7 +30,7 @@ int optimal(string &s){
 int main(){
     string req = "abc";
     string s = "bbacba";
-    // cout<<bruteforce(s , req);
-    cout<<optimal(s);
+    cout<<bruteforce(s);
+    // cout<<optimal(s);
     return 0;
 }
